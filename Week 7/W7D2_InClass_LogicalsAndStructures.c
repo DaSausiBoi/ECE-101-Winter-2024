125e941d951fa0110203042dadb7162d283c17e4
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-22 11:03:26 -0500

    final class code

--- Week 7/W7D2_InClass_LogicalsAndStructures.c
+++ Week 7/W7D2_InClass_LogicalsAndStructures.c
@@ -8,3 +8,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Global variables
+short int myInt = 2;
+short int myInt2 = 1000000000;
+int myInt3 = 1000000000;
+int myDataTypeSize;
+
+char myChar = 'A';
+char newLetter;
+char myLunchSpot[] = {'E', 'i', 'n'};
+char myLunchSpot2 = 'Einstein';
+
+float temperatures[] = {32.4, 40.2, 50, 72, 80, 82};
+
+int main() {
+  // Local variables
+  newLetter = myChar++;
+
+  myDataTypeSize = sizeof(temperatures);
+  printf("The size of the data type chosen is: %d\n", myDataTypeSize);
+
+  printf("The original letter was %c. The new letter is %c.\n", myChar, newLetter);
+  return 0;
+}
