f3e9a817dea750e73e0a4b6dffb16fa1382a81d4
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-22 11:46:29 -0500

    final class code

--- Week 7/W7D2_InClass_Structures.c
+++ Week 7/W7D2_InClass_Structures.c
@@ -11,18 +11,25 @@
 // Global variables
 unsigned short int mixerOn = 1;      // 0 is false, 1 is true
 unsigned short int releaseValveOpen = 1;
+unsigned short int logicalAnswer;
 
 int main() {
   // Local variables
 
-  if (mixerOn){     // Runs if mixer is on
+  if (mixerOn && releaseValveOpen){     // Runs if mixer about to blow up
+    printf("Machine using power.\n");
+  }
+  else if (mixerOn) {     // Runs if mixer on
     printf("Mixer is on.\n");
   }
-  else if(releaseValveOpen) {     // Runs is valve open
+  else if(releaseValveOpen) {     // Runs if valve open
     printf("Release valve open.\n");
   }
-  else {        // Runs if nothing is true
-    printf("Mixer is off.\n");
+  else {        // Runs if nothing true
+    printf("Mixer is off, release valve closed.\n");
   }
+
+  logicalAnswer = mixerOn && !releaseValveOpen;
+  logicalAnswer ? printf("Mixing\n") : printf("Not mixing\n");
   return 0;
 }
