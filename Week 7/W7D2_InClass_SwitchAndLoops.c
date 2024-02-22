c3a1652ae2351d0cd2e0b2d6d23c0bd43b50ee80
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-22 12:08:13 -0500

    final class code

--- Week 7/W7D2_InClass_SwitchAndLoops.c
+++ Week 7/W7D2_InClass_SwitchAndLoops.c
@@ -9,12 +9,29 @@
 #include <stdlib.h>
 
 // Global variables
-int vendingMachineBehavior = 0;
+// int vendingMachineBehavior = 0;
+int squaredInput = 25;
+int sqrtAns;
+int intermediateVals;
 
 int main() {
   // Local variables
 
-    switch(vendingMachineBehavior) {
-        
+    while (intermediateVals < squaredInput) {
+        intermediateVals = intermediateVals + sqrtAns + sqrtAns + 1;
+        sqrtAns++;
     }
+    printf("The square root of %d is %d.\n", squaredInput, sqrtAns);
+
+    // Demo code 1
+    /*  switch(vendingMachineBehavior) {
+        case 1:
+            printf("Waiting\n");
+            break
+        case 2:
+            printf("Shutting down...\n");
+    }   
+    */
+
+    return 0;
 }
