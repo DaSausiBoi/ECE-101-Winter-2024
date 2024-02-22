0b3b49280c10e5b11765896b2cb285717ab3188b
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-22 11:48:18 -0500

    feb 22 24 class code pt 3

---
+++ Week 7/W7D2_InClass_SwitchAndLoops.c
@@ -0,0 +1,20 @@
+/*
+  ECE 101
+  Switch and Loops
+  Feb 22, 2024
+  Loreto Albaran
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variables
+int vendingMachineBehavior = 0;
+
+int main() {
+  // Local variables
+
+    switch(vendingMachineBehavior) {
+        
+    }
+}
