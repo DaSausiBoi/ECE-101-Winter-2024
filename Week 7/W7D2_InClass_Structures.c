89fae46c40284ddb29291879a75c762f999f965d
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-22 11:20:43 -0500

    mid code break

--- Week 7/W7D2_InClass_Structures.c
+++ Week 7/W7D2_InClass_Structures.c
@@ -9,9 +9,20 @@
 #include <stdlib.h>
 
 // Global variables
-
+unsigned short int mixerOn = 1;      // 0 is false, 1 is true
+unsigned short int releaseValveOpen = 1;
 
 int main() {
   // Local variables
-  
+
+  if (mixerOn){     // Runs if mixer is on
+    printf("Mixer is on.\n");
+  }
+  else if(releaseValveOpen) {     // Runs is valve open
+    printf("Release valve open.\n");
+  }
+  else {        // Runs if nothing is true
+    printf("Mixer is off.\n");
+  }
+  return 0;
 }
