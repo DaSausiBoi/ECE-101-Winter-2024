f30cdff4c17da85dbf773d46a286447d9adbe011
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-24 23:49:02 -0500

    file rename + hw code init

--- Week 6/W6D1_Albaran_MathCompilersC.c
+++ Week 6/W6D1_Albaran_MathCompilersC.c
@@ -1,8 +1,12 @@
+/*
+  ECE 101
+  Math and Compilers in C
+  Feb 26, 2024
+  Loreto Albaran
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-#define distance(x1, y1, x2, y2) sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)))
 
 int main () {
     int x1 = 1;
@@ -11,5 +15,4 @@ int main () {
     int y2 = 5;
 
     printf("The distance between (%d, %d) and (%d, %d) is: %0.2f.\n", x1, y1, x2, y2, distance(x1, x2, y1, y2));
-    printf("Hello world!");
 }
