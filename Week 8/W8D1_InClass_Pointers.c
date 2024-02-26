cf73a2d287ed446430c0dcc7ee770ea6d52042cd
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-26 12:06:09 -0500

    final class code

--- Week 8/W8D1_InClass_Pointers.c
+++ Week 8/W8D1_InClass_Pointers.c
@@ -10,8 +10,18 @@
 
 // Global variables
 int myArray[] = {2, 4, 6, 8, 10};
-int *ptr;
+
+int *ptr = &myArray[0];
+int i;      // Loop iteration counter
+int length = sizeof(myArray) / sizeof(int);
 
 int main () {
-    return 0;
+    printf("An int is %d bytes\n\n", sizeof(int));
+
+    for (i = 0; i < length; i++){
+        printf("The address of the array[%d] is %p\n", i, &myArray[i]);
+        printf("ptr has the value %p\n", ptr);
+        printf("Array[%d] holds %d and *ptr points %d\n\n", i, myArray[i], *ptr);
+        ptr++;      // moves ptr one 'int' length further in memory
+    }
 }
