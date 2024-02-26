93fb3cb3c7c06ae27ebd98a6e1cdbdfc8668a1fc
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-26 11:44:35 -0500

    class code init + gitignore add

---
+++ Week 8/W8D1_InClass_Pointers.c
@@ -0,0 +1,17 @@
+/*
+  ECE 101
+  Pointers
+  Feb 26, 2024
+  Loreto Albaran
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variables
+int myArray[] = {2, 4, 6, 8, 10};
+int *ptr;
+
+int main () {
+    return 0;
+}
