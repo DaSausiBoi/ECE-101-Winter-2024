a2c1631e0e857ad8558590dc2ee9c15f4491951f
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-02-26 20:12:44 -0500

    [WIP] added kinematic equation stuff

--- Week 6/W6D1_Albaran_MathCompilersC.c
+++ Week 6/W6D1_Albaran_MathCompilersC.c
@@ -9,11 +9,18 @@
 #include <stdlib.h>
 
 // Global variables
-float var1 = 15.5;
-float var2 = 10.2;
-float ans;
+float xFinal;                       // solving for in meters
+float xInitial = 0;                 // in meters
+float finalVelocity = 16.7;         // in m/s
+float initialVelocity = 0;          // in m/s
+float acceleration = 2.08;          // in m/s^2
+float deltaTime = 8;                // in sec
 
 int main () {
-  ans = var1 + var2;
-  printf("%0.2f + %0.2f equals %0.2f.\n", var1, var2, ans);
+  float moment = (initialVelocity * deltaTime) + ((1 / 2) * acceleration * (deltaTime * deltaTime)) + xInitial;
+  xFinal = moment;
+  printf("%0.2f", xFinal);
+
+  // printf("The final position of the car that started at %d meters going %0.2f m/s^2 over %d seconds is %0.2f meters away.\n", 
+    // xInitial, acceleration, deltaTime, xFinal);
 }
