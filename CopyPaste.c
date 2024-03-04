f8a36685a61f60b337f9eb5b529df546603ce45a
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-03-04 10:40:28 -0500

    copy paste file

---
+++ CopyPaste.c
@@ -0,0 +1,18 @@
+/*
+  ECE 101
+  [TITLE]
+  [DATE]
+  Loreto Albaran
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variables
+
+
+int main () {
+  // Local variables
+
+  return 0;
+}
