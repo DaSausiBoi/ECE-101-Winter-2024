e7995dac131578a2dac0f189a659284b2ece6ea4
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-03-04 10:37:03 -0500

    stopwatch code init + gitignore append

---
+++ Week 9/W9D1_InClass_Stopwatch.c
@@ -0,0 +1,18 @@
+/*
+  ECE 101
+  Stopwatch
+  March 4, 2024
+  Loreto Albaran
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variables
+
+
+int main () {
+  // Local variables
+
+  return 0;
+}
