ae729509d498b5eac6660696ec8d886c83841f17
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-03-18 15:30:15 -0400

    added first code based on stopwatch

--- Week 9/W9D2_Albaran_TrafficLightController.c
+++ Week 9/W9D2_Albaran_TrafficLightController.c
@@ -1,15 +1,106 @@
 /*
   ECE 101
-  Traffic Light Controller
+  Time-Based Traffic Light Controller
   March 24, 2024
   Loreto Albaran
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
 
-// Global var
+// Global variables
+int lightState = 1;
+
+// Flow control variables
+int waitFirst = 1;                              // sets flag for first wait since init
+int timeFirst = 1;                              // sets flag for first timing since init
+
+// Data variable
+int startTime;
+int stopTime;
+int finalTime;
+int timeMs;
+int timeMin;
+int timeSec;
+int doubleSec;
+int singleSec;
 
 int main () {
-  // Local var
+  while (1) {                                   // MAIN LOOP
+    if (lightState == 0) {                      // SHUT STATE
+      printf("Shutting down...\n");
+      Sleep(2000);                              // shutdown time simulation
+      break;
+    }
+    else if (lightState == 1) {                 // INIT STATE
+      printf("I\n");
+      Sleep(1000);                              // init time simulation
+      lightState = 2;                           // INIT --> WAIT
+    }
+
+    else if (lightState == 2) {                 // WAIT STATE
+      if (waitFirst == 1) {                     // first run should print wait once
+        printf("Waiting, press CTRL + ESC to shutdown or CTRL + SHIFT to start stopwatch...\n");
+        waitFirst = 0;                          // prevents "wait" print after first run
+      }
+
+      if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_ESCAPE)) {        // WAIT --> SHUTDOWN
+        lightState = 0;
+      }
+      else if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_LSHIFT) || GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_RSHIFT)) {   // WAIT --> TIMING
+        timeFirst = 1;
+        startTime = clock();
+        lightState = 3;
+      }
+      else {                                    // WAIT --> WAIT
+        lightState = 2;
+      }
+    }
+
+    else if (lightState == 3) {
+      if (timeFirst == 1) {                     // first run should print timing once
+        printf("Stopwatch started, press CTRL + ESC to shutdown or CTRL + SHIFT to stop...\n");
+        timeFirst = 0;                          // prevents "timing" print after first run
+      }
+
+      if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_LSHIFT) || GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_RSHIFT)) {        // TIMING --> WAIT
+        waitFirst = 1;
+        stopTime = clock();
+        finalTime = stopTime - startTime;       // raw time data
+        timeMs = finalTime;                     // time in ms
+
+        while(timeMs >= 100)                    // displays the first two digits in finalTime which represent timeMs
+        {
+          timeMs = timeMs / 10;
+        }
+
+        timeSec = finalTime / CLOCKS_PER_SEC;   // time in sec
+        timeMin = timeSec / 60;                 // time in min
+
+        while (timeSec >= 60) {                 // if seconds are greater than or equal to 60 sec, then subtract value by 60
+          timeSec = timeSec - 60;               // simulates 60 sec turning into 1 min
+        }
+
+        if (timeSec <= 9) {                     // when sec less than 9, prints sec with a 0 beforehand for formatting
+          printf("Total time elapsed: %d:0%d.%d\n\n", timeMin, timeSec, timeMs);
+          Sleep(1000);
+        }
+        else {                                  // else prints sec without the 0 beforehand for formatting
+          printf("Total time elapsed: %d:%d.%d\n\n", timeMin, timeSec, timeMs);
+          Sleep(1000);
+        }
+        lightState = 2;
+      }
+      else if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_ESCAPE)) {   // TIMING --> SHUTDOWN
+        lightState = 0;
+      }
+      else {}                                   // TIMING --> TIMING
+    }
+
+    // "back alley"
+    Sleep(100);                                 // master timer
+  }
 }
