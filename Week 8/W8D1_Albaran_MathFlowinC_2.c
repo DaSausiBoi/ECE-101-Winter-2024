ee74c415f3debf8f46ca158b2790f456501f84e3
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-03-19 18:00:03 -0400

    final code

--- Week 8/W8D1_Albaran_MathFlowinC_2.c
+++ Week 8/W8D1_Albaran_MathFlowinC_2.c
@@ -7,6 +7,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
 
 // Global variables
 int pixelArray1[5][5] = {               // smiley face
@@ -24,32 +27,31 @@ int pixelArray2[5][5] = {               // frowning face
 int keebInput = 0;                      // holds keyboard input value from user
 
 int main () {
-    while (1) {
-        printf("Please enter 0 to see picture 1, 1 to see picture 2, or 2 to quit the program...\n");
+    printf("Please enter 0 to see picture 1, 1 to see picture 2, or 2 to quit the program...\n");
+
+    while (1) {                                                 // MAIN LOOP
         keebInput = getchar();
 
-        // Quits program if 2 is pushed
-        if (keebInput != '2' && keebInput == '0') {
+        if (keebInput != '2' && keebInput == '0') {             // array 1
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    printf("%2d ", pixelArray1[i][j]);
+                    printf("%2d ", pixelArray1[i][j]);          // prints array 1 in 5x5 manner
                 }
                 printf("\n");
             }
+            printf("\nPlease enter 0 to see picture 1, 1 to see picture 2, or 2 to quit the program...\n");
         }
-        else if (keebInput != '2' && keebInput == '1') {
+        else if (keebInput != '2' && keebInput == '1') {        // array 2
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-                    printf("%2d ", pixelArray2[i][j]);
+                    printf("%2d ", pixelArray2[i][j]);          // prints array 2 in 5x5 manner
                 }
                 printf("\n");
             }
+            printf("\nPlease enter 0 to see picture 1, 1 to see picture 2, or 2 to quit the program...\n");
         }
-        else if (keebInput == '2') {
+        else if (keebInput == '2') {                            // SHUTDOWN
             break;
-        }
-        else {
-            printf("Please enter 0 to see picture 1, 1 to see picture 2, or 2 to quit the program...\n");
         }
     }
 }
