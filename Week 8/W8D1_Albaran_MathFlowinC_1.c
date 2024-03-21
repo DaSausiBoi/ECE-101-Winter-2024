3831d89d221e56110492856bf5849f4300fef18c
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2024-03-21 16:00:44 -0400

    changed sphereDiameter == coneDiameter, feet --> inches

--- Week 8/W8D1_Albaran_MathFlowinC_1.c
+++ Week 8/W8D1_Albaran_MathFlowinC_1.c
@@ -11,20 +11,20 @@
 #define M_PI 3.1415926535         // overriding PI definition for this program only (M_PI is defined in math.h already)
 
 // Cone global variables
-double coneDiameter = 15.75;      // in feet
-double coneHeight = 28;           // in feet
-double coneVolume;                // in feet cubed
+double coneDiameter = 15.75;      // in inches
+double coneHeight = 28;           // in inches
+double coneVolume;                // in inches cubed
 
 // Sphere global variables
-double sphereDiameter = 12;       // in feet
-double sphereVolume;              // in feet cubed
+double sphereDiameter = 15.75;    // in inches
+double sphereVolume;              // in inches cubed
 
 int main () {
   coneVolume = ((double) 1 / 3) * M_PI * ((coneDiameter / 2) * 
     (coneDiameter / 2)) * coneHeight;                      // dividing by 2 for radius
-  printf("The volume of a cone that is %3.3f feet wide and %3.3f feet tall is %3.3f feet cubed.\n", coneDiameter, coneHeight, coneVolume);
+  printf("The volume of a cone that is %3.3f inches wide and %3.3f inches tall is %3.3f inches cubed.\n", coneDiameter, coneHeight, coneVolume);
 
   sphereVolume = ((double) 4 / 3) * M_PI * ((sphereDiameter / 2) * 
     (sphereDiameter / 2) * (sphereDiameter / 2));          // dividing by 2 for radius
-  printf("The volume of a sphere that is %3.3f feet wide is %3.3f feet cubed.\n", sphereDiameter, sphereVolume);
+  printf("The volume of a sphere that is %3.3f inches wide is %3.3f inches cubed.\n", sphereDiameter, sphereVolume);
 }
